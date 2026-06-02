cebbd04fdce9006c1d21fcf701ccc85115fe14d4
Christopher Weems <hello@christopherweems.com>
2026-06-02 11:01:23 -0400

    Move `swift-crypto` to the package-suggested dependency range

--- Package.swift
+++ Package.swift
@@ -16,7 +16,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-crypto.git", from: "3.0.0"),
+        .package(url: "https://github.com/apple/swift-crypto.git", "1.0.0" ..< "5.0.0"),
+        
     ],
     targets: [
         .target(
