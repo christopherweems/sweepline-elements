255eb9789d81be6bfa7c4d22bbf1cfc2359c58cc
Christopher Weems <hello@christopherweems.com>
2026-06-20 18:05:47 -0400

    Add `productID` property to `SweetfeetResponse`

--- Sources/Protocol/Sweetfeet/SweetfeetResponse.swift
+++ Sources/Protocol/Sweetfeet/SweetfeetResponse.swift
@@ -1,15 +1,18 @@
 public struct SweetfeetResponse: Codable, Hashable, Sendable {
   public let eventType: SweetfeetEventType
   public let accepted: Bool
+  public let productID: String?
   
-  public init(eventType: SweetfeetEventType, accepted: Bool) {
+  public init(eventType: SweetfeetEventType, accepted: Bool, productID: String?) {
     self.eventType = eventType
     self.accepted = accepted
+    self.productID = productID
   }
   
   enum CodingKeys: String, CodingKey {
     case eventType = "event-type"
     case accepted
+    case productID = "product-id"
   }
 }
 
